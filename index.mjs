cb7665d0f07bac8d0441635a02c39fa39f1f47ed
azotamiota <azotamiota@gmail.com>
2022-10-29 21:06:43 +0100

    setup basic node server

---
+++ index.mjs
@@ -0,0 +1,14 @@
+import express from 'express'
+import os from 'os'
+
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    console.log(`Hello from the ${os.hostname()}`)
+    res.send(`Hello from the ${os.hostname()}`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Web server is listening on ${PORT}`)
+})
